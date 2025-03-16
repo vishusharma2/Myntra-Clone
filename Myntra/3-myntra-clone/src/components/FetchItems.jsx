f134bb716e3d1ef3c3c2d1a0e366f0413ac41d91
Vishu Sharma <42505538+vishusharma2@users.noreply.github.com>
2025-03-16 12:03:06 +0530

    Update FetchItems.jsx

--- Myntra/3-myntra-clone/src/components/FetchItems.jsx
+++ Myntra/3-myntra-clone/src/components/FetchItems.jsx
@@ -14,7 +14,7 @@ const FetchItems = () => {
     const signal = controller.signal;
 
     dispatch(fetchStatusActions.markFetchingStarted());
-    fetch("http://localhost:8080/items", { signal })
+    fetch("https://myntra-clone-one-psi.vercel.app/", { signal })
       .then((res) => res.json())
       .then(({ items }) => {
         dispatch(fetchStatusActions.markFetchDone());
